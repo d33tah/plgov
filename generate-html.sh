84d9c0b976d1dde48fa28cac5f7aa7df3836a40a
Jacek Wielemborek <d33tah@gmail.com>
2015-03-26 18:46:13 +0100

    Rename generuj-html.sh to generate-html.sh, make it use ARGV.

--- generate-html.sh
+++ generate-html.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo '<table border=1>'
-cat plwiki-20150219-pages-meta-history/*-govonly2 | \
+cat $@ | \
     grep -v '^Interesting' | \
     sed -e "s/ u'//g" -e "s/'//g" -e "s/(//g" -e 's/)//g' \
         -e 's/^/<tr><td>/g' -e 's/$/">LINK<\/a><\/td><\/tr>/g' \
