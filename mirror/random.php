296f68c53e411a8b40f4986682d282c553b9807d
Jacek Wielemborek <d33tah@gmail.com>
2015-03-29 01:43:59 +0100

    Testing if I can redirect using .php files.

---
+++ mirror/random.php
@@ -0,0 +1,8 @@
+<HTML>
+<HEAD>
+<META HTTP-EQUIV="refresh" CONTENT="0;https://d33tah.github.com/plgov/random/mirror.html">
+</HEAD>
+<BODY>
+Proszę czekać, trwa przekierowanie do <a href="https://d33tah.github.com/plgov/random/mirror.html"></a>.
+</BODY>
+</HTML>
